3fc2f13d3070cb6930a954ec03d7a796165d85fd
Andrew Li <lawfairachyt28@gmail.com>
2023-02-08 18:30:13 -0500

    Fix hyperparam formatting.
    * add round_size for debugging

--- hyperparam.py
+++ hyperparam.py
@@ -1,5 +1,8 @@
-#number of episode we will run
+#number of episodes we will run
 n_episodes = 10000
+
+#number of episodes in a round
+round_size = 1000
 
 #maximum of iteration per episode
 max_iter_episode = 100
@@ -7,10 +10,10 @@ max_iter_episode = 100
 #initialize the exploration probability to 1
 exploration_proba = 1
 
-#exploartion decreasing decay for exponential decreasing
+#exploration decreasing decay for exponential decreasing
 exploration_decreasing_decay = 0.001
 
-# minimum of exploration proba
+#minimum of exploration proba
 min_exploration_proba = 0.01
 
 #discounted factor
