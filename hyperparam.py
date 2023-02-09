01786d15d8d0688b315ae9a3dd6b0ada0ea94050
Andrew Li <lawfairachyt28@gmail.com>
2023-02-08 19:22:09 -0500

    Format hyperparam.py

--- hyperparam.py
+++ hyperparam.py
@@ -1,3 +1,4 @@
+############################# Model Length Hyperparameters #####################################
 #number of episodes we will run
 n_episodes = 10000
 
@@ -7,17 +8,19 @@ round_size = 1000
 #maximum of iteration per episode
 max_iter_episode = 100
 
+############################# Exploration Hyperparameters #####################################
 #initialize the exploration probability to 1
 exploration_proba = 1
 
-#exploration decreasing decay for exponential decreasing
+#exploration decreasing decay for the exploration exponential decay formula
 exploration_decreasing_decay = 0.001
 
 #minimum of exploration proba
 min_exploration_proba = 0.01
 
-#discounted factor
+############################# Model Training Hyperparameters #####################################
+#discounted factor (lambda) Future rewards are less valuable than current rewards so they must be discounted.
 gamma = 0.99
 
-#learning rate
+#learning rate (alpha)
 lr = 0.1
