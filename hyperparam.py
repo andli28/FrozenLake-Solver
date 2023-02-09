2357d0f5285dd72fbe23746ca9f303795580b37a
Andrew Li <lawfairachyt28@gmail.com>
2023-02-08 19:27:27 -0500

    Further hyperparam.py formatting

--- hyperparam.py
+++ hyperparam.py
@@ -2,20 +2,20 @@
 #number of episodes we will run
 n_episodes = 10000
 
-#number of episodes in a round
+#number of episodes in a round. Therefore, number of rounds is n_episodes/round_size
 round_size = 1000
 
-#maximum of iteration per episode
+#maximum number of iteration per episode
 max_iter_episode = 100
 
 ############################# Exploration Hyperparameters #####################################
-#initialize the exploration probability to 1
+#initialize the exploration probability to 1. This is equivalent to "1 - the greedy policy", or how often we explore (epsilon)
 exploration_proba = 1
 
 #exploration decreasing decay for the exploration exponential decay formula
 exploration_decreasing_decay = 0.001
 
-#minimum of exploration proba
+#minimum of exploration probability
 min_exploration_proba = 0.01
 
 ############################# Model Training Hyperparameters #####################################
